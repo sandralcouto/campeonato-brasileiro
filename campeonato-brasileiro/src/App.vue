479d34b778d9f5a357bf010f68a4ebc656fa9edb
Sandra Couto <sandralmcouto@gmail.com>
2021-09-30 22:25:42 -0300

    adiciona primeira tela

--- campeonato-brasileiro/src/App.vue
+++ campeonato-brasileiro/src/App.vue
@@ -1,43 +1,20 @@
 <template>
   <v-app>
-    <v-app-bar app color="green" dark>
-      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
-      <v-toolbar-title>
-        Campeonato Brasileiro ⚽
-      </v-toolbar-title>
-    </v-app-bar>
-    <v-navigation-drawer v-model="drawer" absolute temporary>
-      <v-list nav dense>
-        <v-list-item-group v-model="group" active-class="green--text text--lighten-2">
- <v-list-item>
-            <v-list-item-icon>
-              <v-icon>mdi-home</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>
-              Início
-            </v-list-item-title>
-          </v-list-item>
-          <v-list-item>
-            <v-list-item-icon>
-              <v-icon>mdi-account</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>
-              Classificação
-            </v-list-item-title>
-          </v-list-item>
-        </v-list-item-group>
-      </v-list>
-    </v-navigation-drawer>
+
+    <AppTopBar/>
+    <Inicio/>
   </v-app>
 </template>
+
 <script>
+import Inicio from './components/Inicio';
+import AppTopBar from './components/shared/AppTopBar';
+
 export default {
   name: 'App',
-  data() {
-    return {
-      drawer: false,
-      group: null,
+  components: {
+      Inicio,
+      AppTopBar
     }
-  }
 };
 </script>
